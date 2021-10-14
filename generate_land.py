61640197b6c2ed2018f96485e200cd9e98acaae2
sodapoph <sodapoph@gmail.com>
2021-10-14 16:48:47 -0500

    updated emojis

---
+++ generate_land.py
@@ -0,0 +1,50 @@
+import random
+from termcolor import colored
+from noise import pnoise2
+
+def generate_land(cols=10, rows=10, noise_level=10):
+    data = ["🌹", "💖", "🌸", "🌝", "🍰", "🦋"]
+    seed = random.randint(0, 100)
+    land = ""
+    
+    print(f"Generate a landscape which is {cols} by {rows}")
+    for row in range(rows):
+        for col in range(cols):
+            n = pnoise2(row / rows, col / cols, base = seed, octaves=5)
+            n *= noise_level
+            n = round(n)
+            n = n % len(data)
+
+            land += data[n]
+        land += "\n"
+           
+    
+    print("finished generating landscape")
+    return land
+
+
+def ask_for_number(question):
+    tries = 0
+
+    while tries < 3:
+        answer = input(colored(question + "\n", "green"))
+
+        if answer == "quit":
+            quit()
+        elif answer.isnumeric():
+            return int(answer)
+        else:
+            print(colored("Oops! Try again with a number.", "yellow"))
+            tries += 1
+
+    print(colored("This isn't fun anymore...", "red"))        
+    quit()
+
+
+if __name__ == "__main__":
+    cols = ask_for_number("How many cols?")
+    rows = ask_for_number("How many rows? ")
+
+    output = generate_land(cols, rows)
+
+    print(output)
---
+++ save_to_file.py
@@ -0,0 +1,14 @@
+from generate_land import ask_for_number, generate_land
+import os
+
+os.makedirs("outputs", exist_ok=True)
+
+noise_levels = [1, 5, 10, 20, 50, 100, 250, 500]
+
+for nl in noise_levels:
+    output = generate_land(200, 200, nl)
+
+    filename = os.path.join("outputs", f"noise-levels-{nl}.txt")
+
+    with open(filename, "w") as f: 
+        f.write(output)
